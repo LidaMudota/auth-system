5d052aec1be3a4844776aaf9fb9bef0a5fca8b71
LidaMudota <ktopro4italtotzdohnet@yandex.ru>
2025-09-16 03:33:48 +0300

    up

--- config/config.php
+++ config/config.php
@@ -1,4 +1,12 @@
 <?php
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$ambientEnv = Dotenv::createImmutable(dirname(__DIR__));
+$ambientEnv->safeLoad();
+
 return [
     'db' => [
         'dsn'  => 'mysql:host=localhost;dbname=auth;charset=utf8mb4',
@@ -7,9 +15,9 @@ return [
     ],
 
     'vk' => [
-        'client_id'     => 'YOUR_VK_CLIENT_ID',
-        'client_secret' => 'YOUR_VK_CLIENT_SECRET',
-        'redirect_uri'  => 'https://your-domain/oauth_vk_callback.php',
+        'client_id'     => $_ENV['VK_CLIENT_ID'] ?? '',
+        'client_secret' => $_ENV['VK_CLIENT_SECRET'] ?? '',
+        'redirect_uri'  => $_ENV['VK_REDIRECT_URI'] ?? '',
     
         'auth_url'      => 'https://id.vk.com/authorize',
         'token_url'     => 'https://id.vk.com/oauth2/auth',
